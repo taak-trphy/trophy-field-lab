b558cdb4259ec9063ee7c9e9cc4d8ab11a9f9184
taak-trphy <tak0726@gmail.com>
2026-09-17 13:35:10 +0900

    Update app.js for Katsushima 2026-09-14 / v1.2.8

--- app.js
+++ app.js
@@ -169,6 +169,6 @@ async function loadData({initial=false}={}){
     if(initial)app.innerHTML=`<div class="error"><strong>データを読み込めませんでした。</strong><p>${esc(err.message)}</p></div>`;
   }
 }
-const currentBuild='1.2.7';
+const currentBuild='1.2.8';
 if(sessionStorage.getItem('trophyBuild')!==currentBuild){sessionStorage.setItem('trophyBuild',currentBuild)}
 loadData({initial:true});
